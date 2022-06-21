b5f2bbd76b9a902928e979d5b2b95d15202d7f0c
Lucianaszwimer <91558510+Lucianaszwimer@users.noreply.github.com>
2022-06-21 09:03:58 -0300

    Add files via upload

---
+++ axios/axiosClient.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { response } from "express";
+
+const axiosClient = axios.create ({
+    baseURL : "http://localhost:5000"
+})
+
+export const getPersonaje = async () => {
+    return axiosClient.get ('/characters').then(response => {
+        if ( response.status < 300){
+            return response.data
+        } else {
+            console.log("Algo anduvo mal")
+        }
+    })
+}
